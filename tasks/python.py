5a12f885421865ab0f848032467a69da88a78713
Timur Yigit <timur@tigitlabs.com>
2025-07-24 09:17:10 +0200

    ci: added mypy

--- tasks/python.py
+++ tasks/python.py
@@ -10,6 +10,14 @@ def lint(c):
 
 
 @task
+def type_check(c):
+    """
+    Run type checking
+    """
+    c.run("poetry run mypy src --config-file .mypy.ini")
+
+
+@task
 def test(c):
     """
     Run tests
@@ -23,10 +31,12 @@ def ci(c):
     Run all CI tasks
     """
     lint(c)
+    type_check(c)
     test(c)
 
 
 python_ns = Collection("python")
 python_ns.add_task(lint, name="lint")
+python_ns.add_task(type_check, name="type_check")
 python_ns.add_task(test, name="test")
 python_ns.add_task(ci, name="ci")
